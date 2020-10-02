aac7a9c9e506127972383d564e32bc7b8fee8052
Kaitlin Phan <kcp2ta@virginia.edu>
2020-10-01 22:34:39 -0400

    Cards implementation

--- meetupfinder/settings.py
+++ meetupfinder/settings.py
@@ -143,9 +143,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/app/static/',
-]
 django_heroku.settings(locals())
 
