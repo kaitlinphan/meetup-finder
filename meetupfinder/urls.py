30a9ff2ac8aecb18aaca6c072950eb88a399cc04
Kaitlin Phan <kcp2ta@virginia.edu>
2020-10-01 22:32:39 -0400

    Cards implementation

--- meetupfinder/urls.py
+++ meetupfinder/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 from . import views
@@ -26,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/',  views.welcome, name='welcome'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
