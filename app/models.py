380e353bcacfc67d4d0a3cfcb7d8ecafbe68fc9d
mavenkim2 <mavenkim2@gmail.com>
2020-10-12 00:42:34 -0400

    Implementing cards

--- app/models.py
+++ app/models.py
@@ -8,4 +8,4 @@ class Event(models.Model):
     title = models.CharField('Title',max_length=200)
     location = models.CharField('Location', max_length=150)
     info = models.TextField()
-    image = models.FilePathField(path="/img")
+    image = models.FilePathField()
--- meetupfinder/settings.py
+++ meetupfinder/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../app/static')
 STATIC_URL = '/static/'
 try:
     # Configure Django App for Heroku.
