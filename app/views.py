7217bcc98fc795caf1425bb283f4ef833753a36e
mavenkim2 <mavenkim2@gmail.com>
2020-10-19 11:55:47 -0400

    Create event

--- app/views.py
+++ app/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from app.models import Event
+from app.forms import EventForm
 import datetime
 
 # Create your views here.
 class HomeView(generic.ListView):
     template_name = 'app/home.html'
     context_object_name = 'event_list'
+
 
 def event_index(request):
     events = Event.objects.all()
@@ -16,9 +18,22 @@ def event_index(request):
     }
     return render(request, 'app/event_index.html', context)
 
+
 def event_detail(request, pk):
     event = Event.objects.get(pk=pk)
     context = {
         'event': event
     }
     return render(request, 'app/event_detail.html', context)
+
+
+def create_event(request):
+    message = ""
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            message = "Comment posted successfully!"
+    else:
+        message = "Fill out all fields before submitting."
+    return render(request, 'app/create_event.html', {'form': EventForm(), 'message': message})
