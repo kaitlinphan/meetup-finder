4505c064f8feed7b7bdd091c1b79d3023e2b5f10
bm2pr <54745911+bm2pr@users.noreply.github.com>
2020-10-25 21:57:04 -0400

    jwekl

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.event_index, name='event_index'),
     path('<int:pk>/', views.event_detail, name='event_detail'),
-<<<<<<< HEAD
     path('create/', views.create_event, name='create_event'),
-=======
 
     path('<int:pk>/register/', views.register, name='register'),
->>>>>>> bm2pr
 ]
--- app/views.py
+++ app/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from app.models import Event
-from app.forms import EventForm
+
+from userAccount.models import User
+
 import datetime
 
 # Create your views here.
 class HomeView(generic.ListView):
     template_name = 'app/home.html'
     context_object_name = 'event_list'
-
 
 def event_index(request):
     events = Event.objects.all()
@@ -18,7 +19,6 @@ def event_index(request):
     }
     return render(request, 'app/event_index.html', context)
 
-
 def event_detail(request, pk):
     event = Event.objects.get(pk=pk)
     context = {
@@ -26,8 +26,6 @@ def event_detail(request, pk):
     }
     return render(request, 'app/event_detail.html', context)
 
-
-<<<<<<< HEAD
 def create_event(request):
     message = ""
     if request.method == "POST":
@@ -38,7 +36,7 @@ def create_event(request):
     else:
         message = "Fill out all fields before submitting."
     return render(request, 'app/create_event.html', {'form': EventForm(), 'message': message})
-=======
+
 
 def register(request, pk):
     event = Event.objects.get(pk=pk)
@@ -46,4 +44,3 @@ def register(request, pk):
         'event': event
     }
     return render(request, 'app/welcome.html', context)
->>>>>>> bm2pr
