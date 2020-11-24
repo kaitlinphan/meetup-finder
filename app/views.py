556d5410914586e1e032d8065efff7977de8f06b
mavenkim2 <mavenkim2@gmail.com>
2020-11-24 07:15:37 -0500

    Hosting events

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from app.models import Event, RegisteredEvents
+from app.models import Event, RegisteredEvents, CreatedEvents
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import logout
@@ -18,8 +18,10 @@ def event_index(request):
 
 def event_detail(request, pk):
     event = Event.objects.get(pk=pk)
+    attendees = RegisteredEvents.objects.filter(event=event)
     context = {
         'event': event,
+        'attendees': attendees
     }
     return render(request, 'app/event_detail.html', context)
 
@@ -55,14 +57,32 @@ def create_event(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('app:event_index'))
     if request.method == "POST":
-        form = EventForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('app:event_index'))
+        title = request.POST['title']
+        day = request.POST['date']
+        if title == '' or day == "":
+            context = {
+                'failed': True
+            }
+            return render(request, 'app/create_event.html', context)
+        info = request.POST['info']
+        location = request.POST['location']
+        image = request.FILES.get('image', False)
+
+        event = Event.objects.create(title=title, day=day)
+        if not info == '':
+            event.info = info
+        if not location == '':
+            event.location = location
+        if image:
+            event.image = image
+        event.poster = request.user.get_full_name()
+        event.save()
+        created_event = CreatedEvents(event=event, user=request.user)
+        created_event.save()
+        return HttpResponseRedirect(reverse('app:event_index'))
     else:
-        form = EventForm()
         context = {
-            'form': form,
+            'failed': False
         }
         return render(request, 'app/create_event.html', context)
 
@@ -73,10 +93,12 @@ def register(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     # adds event to list of registered events
     if RegisteredEvents.objects.filter(event=event, user=request.user).exists():
+        attendees = RegisteredEvents.objects.filter(event=event)
+
         context = {
             'event': event,
             'error_message': "Already registered!",
-            'logged_in': True
+            'attendees': attendees
         }
         return render(request, 'app/event_detail.html', context)
     registered_event = RegisteredEvents(event=event, user=request.user)
@@ -97,8 +119,10 @@ def profile(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('app:event_index'))
     registered_events = RegisteredEvents.objects.filter(user=request.user)
+    created_events = CreatedEvents.objects.filter(user=request.user)
     context = {
-        'events': registered_events,
+        'registered_events': registered_events,
+        'created_events': created_events
     }
     return render(request, 'app/profile.html', context)
 
@@ -109,7 +133,7 @@ def search(request):
         events_check_info = Event.objects.filter(info__icontains=query_string)
         events_check_title = Event.objects.filter(title__icontains=query_string)
         events_check_date = Event.objects.filter(day__icontains=query_string)
-        events_check_location = Event.objects.filter(address__icontains=query_string)
+        events_check_location = Event.objects.filter(location__icontains=query_string)
         events = events_check_info | events_check_title | events_check_date | events_check_location
         return render(request, 'app/event_index.html', {'events': events})
     else:
