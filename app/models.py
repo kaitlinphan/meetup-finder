75d07e2dd61eeaaf82379a91d734087140369a1b
mavenkim2 <mavenkim2@gmail.com>
2020-11-24 12:25:03 -0500

    Cloudinary storage

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Event(models.Model):
     title = models.CharField('Title', max_length=200)
     day = models.DateField('Day', default=datetime.date.today, help_text='YYYY-MM-DD')
     info = models.TextField(blank=True)
-    image = models.ImageField(upload_to="images", blank=True)
+    image = models.FileField(upload_to="images/", blank=True)
     location = models.CharField('Location', default='', max_length=500, blank=True)
     poster = models.CharField('Poster', default='', max_length=25)
 
